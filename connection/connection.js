52b0bd3cf5f485d7363653755ff2343c79135913
User <bheemverma554@gmail.com>
2023-04-17 11:59:43 +0530

    1

--- connection/connection.js
+++ connection/connection.js
@@ -1,21 +1,23 @@
 const mongoose = require("mongoose")
 
 const dotenv = require('dotenv');
-
+dotenv.config();
 
 
 mongoose.connect(
+
+    // config.DB_URL,{useNewUrlParser:true})
     
         process.env.DB_URL,
         {useNewUrlParser:true,useUnifiedTopology: true},
-        err =>{
-            if(err){
-                throw err.message;
-            }
-            else{
-                console.log("databse connection successfully !");
-            }
-        }
-    )
+    //     err =>{
+    //         if(err){
+    //             throw err.message;
+    //         }
+    //         else{
+    //             console.log("databse connection successfully !");
+    //         }
+    //     }
+     )
 
     module.exports = mongoose;
